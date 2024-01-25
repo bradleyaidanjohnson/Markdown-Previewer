413a3757f8ada05977b9b2fdc9cc7f5e99a50f03
bradleyaidanjohnson <bradleyaidanjohnson@gmail.com>
2024-01-25 12:05:59 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <textarea id="editor" />
+      <p id="preiew"></p>
+    </div>
+  );
+}
+
+export default App;
