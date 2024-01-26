abd2aad8c6d65c2a2b11313ee15471db68ba3ad2
bradleyaidanjohnson <bradleyaidanjohnson@gmail.com>
2024-01-26 06:28:54 +0000

    working passes all tests, just CSS to finalise

--- src/App.js
+++ src/App.js
@@ -4,16 +4,10 @@ import Footer from "./Footer";
 import "./index.css";
 
 function App() {
-  const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  };
   return (
     <div className="App">
       <Header />
       <Textarea />
-      <p id="preview">Hello {handleNameChange()}!</p>
       <Footer />
     </div>
   );
--- src/Textarea.js
+++ src/Textarea.js
@@ -1,14 +1,51 @@
-import { setSelectionRange } from "@testing-library/user-event/dist/utils";
 import { useState } from "react";
 import Markdown from "marked-react";
 
 const Textarea = () => {
-  const markdownPreview =
-    "#This is a h1 Heading &#10; ##This is a h2 Sub Heading &#10;a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.";
+  const markdownPreview = `
+# This is a h1 Heading
+
+## This is a h2 Sub Heading
+
+This is a [link to my gitHub](https://github.com/bradleyaidanjohnson)
+
+This is inline code: \`console.log("Hello World")\`
+
+This is a code block:
+\`\`\`
+const a = 1
+const b = 2
+const c = a + b
+console.log(c) // Expected output: 3
+\`\`\`
+
+These are list items with indented items:
+
+1. OL item 1
+   * UL Item 1.1
+   * UL Item 1.2
+   * UL Item 1.3
+2. OL item 2
+   * UL Item 2.1
+   * UL Item 2.2
+   * UL Item 2.3
+
+This is a blockquote:
+> I attribute my success to this - I never gave or took any excuse.
+
+This is an image of meeeeeee:
+
+![image of Bradley Aidan Johnson](https://avatars.githubusercontent.com/u/9921622?v=4)
+
+This is **bolded text**
+`;
   const [textBox, setTextBox] = useState(markdownPreview);
   const updateTextbox = (e) => {
     const userText = e.target.value;
-    setTextBox(e.target.value);
+    // Add formatting for a space between # and characters
+    const carriageFormatted = userText.replace(/[\n\r]/g, "  \n");
+    console.log(carriageFormatted);
+    setTextBox(carriageFormatted);
   };
 
   return (
@@ -16,7 +53,7 @@ const Textarea = () => {
       <textarea
         id="editor"
         onChange={(e) => updateTextbox(e)}
-        placeholder="#This is a h1 Heading &#10; ##This is a h2 Sub Heading &#10;"
+        placeholder={markdownPreview}
       />
       <div id="preview">
         <Markdown>{textBox}</Markdown>
